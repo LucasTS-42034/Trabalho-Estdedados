55752254026eff2e47b9955377ed6643355736b8
LucasT <tedescosantoslucas955@gmail.com>
2026-05-01 07:46:30 -0300

    Commit

--- velha.py
+++ velha.py
@@ -1,3 +1,83 @@
+def tabu(tab):
+    print(f"\n {tab[0]} | {tab[1]} | {tab[2]} ")
+    print("---+---+---")
+    print(f" {tab[3]} | {tab[4]} | {tab[5]} ")
+    print("---+---+---")
+    print(f" {tab[6]} | {tab[7]} | {tab[8]} \n")
+
+
+def vitoria(tab, joga):
+    for i in range(0, 9, 3):
+        if tab[i] == tab[i+1] == tab[i+2] == joga:
+            return True
+    for i in range(3):
+        if tab[i] == tab[i+3] == tab[i+6] == joga:
+            return True
+    if tab[0] == tab[4] == tab[8] == joga:
+        return True
+    if tab[2] == tab[4] == tab[6] == joga:
+        return True
+    return False
+
+
+def pvp():
+    
+    tab = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
+    sim1 = input("X ou O? \n").upper()
+    sim2 = 0
+    if sim1 == "X":
+        sim2 = "O"
+    elif sim1 == "O":
+        sim2 = "X"
+    else:
+        print("Escolha inválida! Será usado X.")
+        sim1 = "X"
+        sim2 = "O"
+    
+    print("Jogador 1: " + sim1)
+    print("Jogador 2: " + sim2)
+    
+    joga = sim1     
+    rodadas = 0     
+    
+    
+    while rodadas < 9:
+        tabu(tab)
+        print(f"Vez do jogador {joga}:")
+        
+        try:
+            vez = int(input("Digite a posição que quer jogar (de 1-9): \n")) - 1
+            
+            if vez < 0 or vez > 8:
+                print("Posição inválida! Escolha entre 1 e 9.")
+                continue
+                
+            if tab[vez] in ["X", "O"]:
+                print("Posição já ocupada!")
+                continue
+            
+            tab[vez] = joga
+            rodadas += 1
+            
+            if vitoria(tab, joga):
+                tabu(tab)
+                print(f"Jogador {joga} venceu! 🎉")
+                return
+            
+            if joga == sim1:
+                joga = sim2
+            else:
+                joga = sim1
+                
+        except ValueError:
+            print("Digite um número válido!")
+    
+    tabu(tab)
+    print("Empate!")
+
+
+
 print("---- Jogo da velha ----")
 while True:
     print("\n Escolha: ")
@@ -10,18 +90,8 @@ while True:
         print("Até logo!")
         break
     elif opcao == "1":
-        tabuleiro = [[0], [0], [0], 
-                     [0], [0], [0], 
-                     [0], [0], [0]]
-        sim1 = input("X ou O? \n").upper()
-        sim2 = 0
-        if sim1 == "X":
-            sim2 = "O"
-        elif sim1 == "O":
-            sim2 = "X"
-        
-        for i in range(9):
-            print(tabuleiro[0:3]) 
-            print(tabuleiro[3:6]) 
-            print(tabuleiro[6:9])            
-            jogada = int(input("Digite onde quer jogar: \n"))
+        pvp()
+    elif opcao == "2":
+        print("Modo PVE")
+    else:
+        print("Opção inválida!")
